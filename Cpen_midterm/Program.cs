2ed1a13e4ba1c0b5e90004f3dc313655c8f41513
Stacy <sxy20000401@gmail.com>
2020-11-07 01:02:12 +0800

    fixed minor mistake

--- Cpen_midterm/Program.cs
+++ Cpen_midterm/Program.cs
@@ -66,7 +66,7 @@ namespace MTQ1
 
              Task.WaitAll(tasks2);
              */
-            //  Console.WriteLine("Counter:{0}", counter);
+              Console.WriteLine("Counter:{0}", counter);
 
 
 
